307050a3ca72e8fa2c1bba1366f00c962389836f
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-10-01 15:03:39 -0400

    Question 1

--- Sassy/main.js
+++ Sassy/main.js
@@ -1,19 +1,57 @@
 (function (){
 
 
-console.log(items)
-var priceArray = [];
+// items.forEach ( function(item){
+// console.log(item.price);
+
+// }); 
+
+var prices = items.map( function(item){
+  return item.price;
+
+});
+ console.log(prices);
+ 
+ //Add all the prices together
+ var sum = prices.reduce(function(previous, next){
+  return previous + next;
+
+});
+console.log(sum);
+
+//Divide by total number of items
+var avg = sum / items.length;
+
+console.log(avg);
+
+//Convert to 2 decimal places
+var converted = avg.toFixed(2);
+
+console.log(converted);
+
+//Make it a string
+ var str = 'The average price is ' + converted;
+
+//Make it show up on the page
+var answerOne = document.querySelector('#answerOne')
+var textNode = document.createTextNode(str);
+
+answerOne.appendChild(textNode);
+
+
+// console.log(items)
+// var priceArray = [];
 
 
 
-items.map(function(){
-  for(i =0; i < items.length; i++)
-  var item = items[i].price * parseInt(items[i].quantity);
-  priceArray.push(item);
+// items.map(function(){
+//   for(i =0; i < items.length; i++)
+//   var item = items[i].price * parseInt(items[i].quantity);
+//   priceArray.push(item);
 
-})
+// })
 
 
-console.log(priceArray);
+// console.log(priceArray);
 
 })();
